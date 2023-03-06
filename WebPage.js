79d70bfde89396db32dadb855025dbc88eac27b4
Mahesh <mahesh.guntumadugu@delta.com>
2023-03-06 22:28:59 +0530

    changes

--- WebPage.js
+++ WebPage.js
@@ -55,9 +55,9 @@ const BasePage = function (customAudio = null) {
     const initialLoginClickJsCode = "document.querySelector(`*[slot='globalheader']`).getElementsByClassName('mr-0 mt-2 mb-4 mt-lg-0 mb-lg-0').item(0).firstChild.click();";  
     await this.driver.executeScript(initialLoginClickJsCode);
     await this.driver.sleep(2000);
-    const inputUserNameJs = "document.querySelector(`*[slot='globalheader']`).getElementsByClassName('sub2').item(0).children[0].value='8006695202'"
+    const inputUserNameJs = "document.querySelector(`*[slot='globalheader']`).getElementsByClassName('sub2').item(0).children[0].value='test'"
     await this.driver.executeScript(inputUserNameJs); 
-    const inputPasswordJs = "document.querySelector(`*[slot='globalheader']`).getElementsByClassName('sub2').item(0).children[1].value = 'Delta999'"
+    const inputPasswordJs = "document.querySelector(`*[slot='globalheader']`).getElementsByClassName('sub2').item(0).children[1].value = 'test'"
     await this.driver.executeScript(inputPasswordJs); 
     const finalLoginJs = "document.querySelector(`*[slot='globalheader']`).getElementsByClassName('sub4').item(0).children[0].click();"
     await this.driver.executeScript(finalLoginJs);
